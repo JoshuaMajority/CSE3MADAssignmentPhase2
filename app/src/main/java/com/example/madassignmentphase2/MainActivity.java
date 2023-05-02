d6f83835232398dde62c4a84334898b7c1855194
JoshuaMajority <typerrapid@gmail.com>
2023-05-02 11:26:54 +1000

    testing

--- app/src/main/java/com/example/madassignmentphase2/MainActivity.java
+++ app/src/main/java/com/example/madassignmentphase2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.madassignmentphase2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        Button btn = findViewById(R.id.btnok);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "The Button has been Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
