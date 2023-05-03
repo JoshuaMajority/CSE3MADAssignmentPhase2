28f9cb7f23f5da095f2ecc44cd56ce6ccc6bd3f7
JoshuaMajority <typerrapid@gmail.com>
2023-05-03 21:21:55 +1000

    testing

---
+++ app/src/main/java/com/example/madassignmentphase2/Add_Invoice.java
@@ -0,0 +1,14 @@
+package com.example.madassignmentphase2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Invoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_invoice);
+    }
+}
---
+++ app/src/main/java/com/example/madassignmentphase2/Add_New_Item.java
@@ -0,0 +1,14 @@
+package com.example.madassignmentphase2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_New_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_item);
+    }
+}
