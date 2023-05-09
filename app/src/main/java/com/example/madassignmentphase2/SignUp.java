318324bbc61f080f3a88c1fa877769d721eada13
JoshuaMajority <typerrapid@gmail.com>
2023-05-09 12:42:03 +1000

    testing

--- app/src/main/java/com/example/madassignmentphase2/SignUp.java
+++ app/src/main/java/com/example/madassignmentphase2/SignUp.java
@@ -2,7 +2,10 @@ package com.example.madassignmentphase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_page);
+
+        Button btn = findViewById(R.id.Sign_Up);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myint = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(myint);
+            }
+        });
     }
 }
