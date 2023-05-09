ca4c0843566bcbddea69a8197d70bc3e8f1746da
JoshuaMajority <typerrapid@gmail.com>
2023-05-09 12:03:44 +1000

    testing

--- app/src/main/java/com/example/madassignmentphase2/Home_Page.java
+++ app/src/main/java/com/example/madassignmentphase2/Home_Page.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class Home_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button btn = findViewById(R.id.addin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "The Button has been Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
