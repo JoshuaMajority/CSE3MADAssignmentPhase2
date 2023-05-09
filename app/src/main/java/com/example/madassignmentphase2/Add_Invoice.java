91bf16a0e6c079c23ab3fd23a24515928b04fde2
JoshuaMajority <typerrapid@gmail.com>
2023-05-09 12:46:17 +1000

    testing

--- app/src/main/java/com/example/madassignmentphase2/Add_Invoice.java
+++ app/src/main/java/com/example/madassignmentphase2/Add_Invoice.java
@@ -15,12 +15,20 @@ public class Add_Invoice extends AppCompatActivity {
         setContentView(R.layout.activity_add_invoice);
 
         Button btn = findViewById(R.id.cai);
+        Button two = findViewById(R.id.addnewi);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myInt = new Intent(getApplicationContext(), Home_Page.class);
                 startActivity(myInt);
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent my2Int = new Intent(getApplicationContext(), Add_New_Item.class);
+                startActivity(my2Int);
             }
         });
     }
