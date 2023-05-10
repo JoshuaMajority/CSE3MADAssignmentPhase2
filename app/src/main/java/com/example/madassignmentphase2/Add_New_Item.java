598f4fa0f0dadeb4b419950013a3b53b0e9f63b4
JoshuaMajority <typerrapid@gmail.com>
2023-05-11 09:34:16 +1000

    testing

--- app/src/main/java/com/example/madassignmentphase2/Add_New_Item.java
+++ app/src/main/java/com/example/madassignmentphase2/Add_New_Item.java
@@ -2,7 +2,10 @@ package com.example.madassignmentphase2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Add_New_Item extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Add_New_Item extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
+
+        Button btn = findViewById(R.id.close);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myInt = new Intent(getApplicationContext(), Home_Page.class);
+                startActivity(myInt);
+            }
+        });
     }
 }
